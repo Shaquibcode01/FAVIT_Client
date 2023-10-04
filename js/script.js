c100aaabd164bf8be57317f1212d7282ef300b76
Shaquib Khan <shaquibkhan2003@gmail.com>
2023-10-05 01:57:01 +0530

    Nav-Bar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function toggleNavbar() {
+    const navbar = document.querySelector('.navbar');
+    navbar.style.display = navbar.style.display === 'none' || navbar.style.display === '' ? 'block' : 'none';
+
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('active', navbar.style.display === 'block');
+}
