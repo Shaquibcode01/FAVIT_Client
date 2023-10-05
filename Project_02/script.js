c728fbccce8bd753c1e443fc055dba6bd9a05470
Shaquib Khan <shaquibkhan2003@gmail.com>
2023-10-05 18:48:07 +0530

    First

--- Project_02/script.js
+++
@@ -1,7 +0,0 @@
-function toggleNavbar() {
-    const navbar = document.querySelector('.navbar');
-    navbar.style.display = navbar.style.display === 'none' || navbar.style.display === '' ? 'block' : 'none';
-
-    const menuIcon = document.querySelector('.menu-icon');
-    menuIcon.classList.toggle('active', navbar.style.display === 'block');
-}
